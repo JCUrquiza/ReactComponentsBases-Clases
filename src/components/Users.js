bed6dff86d21d0e866b989cb3e922eed6f89172c
Juan <gurquiza8@gmail.com>
2023-05-29 18:18:38 -0500

    using something sims like hooks and super on the constructor

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { Component, useState } from 'react';
 import User from './User';
 
 import classes from './Users.module.css';
@@ -9,6 +9,45 @@ const DUMMY_USERS = [
   { id: 'u3', name: 'Julie' },
 ];
 
+class Users extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      showUsers: true
+    };
+  }
+
+  toggleUsersHandler() {
+    // this.state.showUsers = false // NOT!
+    this.setState((currentState) => {
+      return { showUsers: !currentState.showUsers }
+    });
+  }
+
+  render() {
+
+    const usersList = (
+      <ul>
+        {DUMMY_USERS.map((user) => (
+          <User key={user.id} name={user.name} />
+        ))}
+      </ul>
+    );
+
+    return (
+      <div className={classes.users}>
+        <button onClick={this.toggleUsersHandler.bind(this)}>
+          {this.state.showUsers ? 'Hide' : 'Show'} Users
+        </button>
+        {this.state.showUsers && usersList}
+      </div>
+    );
+  }
+
+}
+
+/*
 const Users = () => {
   const [showUsers, setShowUsers] = useState(true);
 
@@ -33,5 +72,7 @@ const Users = () => {
     </div>
   );
 };
+*/
 
 export default Users;
+
